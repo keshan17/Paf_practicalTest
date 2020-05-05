109a3dc0581fefb67db72a6b7001b6360f89d942
keshan17 <keshanweragoda1@gmail.com>
2020-05-06 03:55:41 +0530

    updated js file

--- WebContent/Components/main.js
+++ WebContent/Components/main.js
@@ -1,8 +1,7 @@
 //hide alert
 $(document).ready(function() {
-	if ($("#alertSuccess").text().trim() == "") {
-		$("#alertSuccess").hide();
-	}
+
+	$("#alertSuccess").hide();
 	$("#alertError").hide();
 });
 
@@ -20,19 +19,85 @@ $(document).on("click", "#submitusers", function(event) {
 		return;
 	}
 	// If valid------------------------
-	$("#usersForm").submit();
+	var type = ($("#hidUsersIDSave").val() == "") ? "POST" : "PUT";
+
+	$.ajax({
+		url : "AdminAPI",
+		type : type,
+		data : $("#usersForm").serialize(),
+		dataType : "text",
+		complete : function(response, status) {
+			onItemSaveComplete(response.responseText, status);
+		}
+	});
+
 });
+
+function onItemSaveComplete(response, status) {
+	if (status == "success") {
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success") {
+			$("#alertSuccess").text("Successfully saved.");
+			$("#alertSuccess").show();
+			$("#divDocGrid").html(resultSet.data);
+			$("#divPatientGrid").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error") {
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	} else if (status == "error") {
+		$("#alertError").text("Error while saving.");
+		$("#alertError").show();
+	} else {
+		$("#alertError").text("Unknown error while saving..");
+		$("#alertError").show();
+	}
+	$("#hidUsersIDSave").val("");
+	$("#usersForm")[0].reset();
+}
+
+$(document).on("click", ".btnRemove", function(event) {
+	$.ajax({
+		url : "AdminAPI",
+		type : "DELETE",
+		data : "did=" + $(this).data("itemid"),
+		dataType : "text",
+		complete : function(response, status) {
+			onItemDeleteComplete(response.responseText, status);
+		}
+	});
+});
+
+function onItemDeleteComplete(response, status) {
+	if (status == "success") {
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success") {
+			$("#alertSuccess").text("Successfully deleted.");
+			$("#alertSuccess").show();
+			$("#divDocGrid").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error") {
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	} else if (status == "error") {
+		$("#alertError").text("Error while deleting.");
+		$("#alertError").show();
+	} else {
+		$("#alertError").text("Unknown error while deleting..");
+		$("#alertError").show();
+	}
+}
+
 // UPDATE==========================================
-$(document).on(
-		"click",
-		".btnUpdate",
-		function(event) {
-			$("#hidItemIDSave").val($(this).closest("tr").find('#hidUserIDUpdate').val());
+$(document).on("click",".btnUpdate",function(event)
+		{
+			$("#hidUserIDUpdate").val(
+					$(this).closest("tr").find('#hidUserIDUpdate').val());
 			$("#name").val($(this).closest("tr").find('td:eq(0)').text());
 			$("#nic").val($(this).closest("tr").find('td:eq(1)').text());
 			$("#email").val($(this).closest("tr").find('td:eq(2)').text());
 			$("#pwd").val($(this).closest("tr").find('td:eq(3)').text());
-			$("#status").val($(this).closest("tr").find('td:eq(4)').text());
+			// $("#status").val($(this).closest("tr").find('td:eq(4)').text());
 		});
 // CLIENTMODEL=========================================================================
 function validateItemForm() {
@@ -48,7 +113,7 @@ function validateItemForm() {
 	if ($("#email").val().trim() == "") {
 		return "Insert user email.";
 	}
-	
+
 	// password-----------------------
 	if ($("#pwd").val().trim() == "") {
 		return "Insert user password.";
